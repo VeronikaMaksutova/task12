70e207df2d286fc15eff68d9fd6c20023954611b
VeronikaMaksutova <maksutovy33@gmail.com>
2021-11-22 22:18:26 +0300

    Initial commit

---
+++ src/com/company/Student.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Student {
+
+    Scanner in=new Scanner(System.in);
+    private int id, size;
+    private String FIO;
+
+    Student(int id, String FIO)
+    {
+        this.FIO=FIO;
+        this.id=id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getFIO()
+    {
+        return FIO;
+    }
+}
